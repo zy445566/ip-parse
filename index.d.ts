ae52d36cf446989154ab21f056432a654f05e483
zy445566 <zyct@vip.qq.com>
2020-01-14 14:22:58 +0800

    add d.ts

---
+++ index.d.ts
@@ -0,0 +1,7 @@
+declare class IpParse {
+    isIp(ip:string):number;
+    isIpv4(ip:string):boolean;
+    isIpv6(ip:string):boolean;
+    parseIp(ip:string):Array<string>;
+}
+export default new IpParse()
